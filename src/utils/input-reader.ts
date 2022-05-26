38f69add6e1323830218acbb02a3728dabf26548
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-05-26 23:32:25 +0200

    Add input reader

---
+++ src/utils/input-reader.ts
@@ -0,0 +1,28 @@
+const lineByLine = require('n-readlines');
+
+
+export class InputReader {
+
+    private reader: any;
+
+    constructor(pathToFile: string) {
+        this.reader = new lineByLine(pathToFile);
+    }
+
+    public readLineAsString(): string {
+        return this.reader.next();
+    }
+
+    public readLineAsStringArray(splitter: string = ' '): string[] {
+        return this.readLineAsString().split(splitter);
+    }
+
+    public readLineAsNumber(): number {
+        return +this.readLineAsString();
+    }
+
+    public readLineAsNumberArray(splitter: string = ' '): number {
+        return +this.readLineAsStringArray(splitter).map(s => +s);
+    }
+
+}
