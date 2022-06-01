f4a89534ee098789caf853f16480ed99bbea00c8
Ingo Schwarz <ingo.schwarz23@gmail.com>
2022-06-01 22:06:57 +0200

    Fix input reader

--- src/utils/input-reader.ts
+++ src/utils/input-reader.ts
@@ -1,16 +1,24 @@
-const lineByLine = require('n-readlines');
+import LineByLine from 'n-readlines';
 
 
 export class InputReader {
 
-    private reader: any;
+    private reader: LineByLine;
+
+    private currentLine: Buffer | false | null = null;
 
     constructor(pathToFile: string) {
-        this.reader = new lineByLine(pathToFile);
+        this.reader = new LineByLine(pathToFile);
     }
 
     public readLineAsString(): string {
-        return this.reader.next().toString('utf8');
+        this.loadNextLineIfLastLineWasProcessed();
+        if (this.currentLine) {
+            const line = this.currentLine.toString('utf8');
+            this.markCurrentLineAsProcessed();
+            return line;
+        } 
+        throw new Error("File ending reached.")
     }
 
     public readLineAsStringArray(splitter: string = ' '): string[] {
@@ -21,8 +29,22 @@ export class InputReader {
         return +this.readLineAsString();
     }
 
-    public readLineAsNumberArray(splitter: string = ' '): number {
-        return +this.readLineAsStringArray(splitter).map(s => +s);
+    public readLineAsNumberArray(splitter: string = ' '): number[] {
+        return this.readLineAsStringArray(splitter).map(s => +s);
+    }
+
+    get hasMoreLines(): boolean {
+        this.loadNextLineIfLastLineWasProcessed();
+        return this.currentLine != false;
+    }
+
+    private loadNextLineIfLastLineWasProcessed(): void {
+        if(!this.currentLine)
+            this.currentLine = this.reader.next();
+    }
+    
+    private markCurrentLineAsProcessed() {
+        this.currentLine = null;
     }
 
 }
